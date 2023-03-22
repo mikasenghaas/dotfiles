8185bc7d5daa52b20bc7d196f1a233cc0cca924e
mikasenghaas <accounts@jonassenghaas.de>
2023-03-22 11:04:56 +0100

    feat(nvim): vimtex auto compiles to build directory

--- .config/nvim/after/plugin/vimtex.lua
+++ .config/nvim/after/plugin/vimtex.lua
@@ -1,6 +1,13 @@
+-- set skim as pdf viewer
 vim.g.vimtex_view_method = "skim"
 vim.g.vimtex_view_general_viewer = "skim"
 vim.g.vimtex_view_forward_search_on_start = true
 
+-- compilation options
+vim.g.vimtex_compiler_latexmk = {
+  build_dir = 'build'
+}
+
+-- keymaps for auto compile and forward search
 vim.keymap.set('n', '<localleader>c', "<plug>(vimtex-compile)")
 vim.keymap.set('n', '<localleader>v', "<plug>(vimtex-view)")
