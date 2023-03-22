a7285ed087c4e0a5b9416a9373425a52f8e83d36
mikasenghaas <accounts@jonassenghaas.de>
2023-03-22 01:49:18 +0100

    feat(nvim): filetype based configs for tex

---
+++ .config/nvim/ftplugin/tex.lua
@@ -0,0 +1,4 @@
+-- settings if filetype tex
+vim.opt.textwidth = 80    -- 80 textwidth
+vim.opt.colorcolumn = "+1"-- colorcolumn one char after textwidth
+vim.opt.wrap = true       -- soft line wrapping
