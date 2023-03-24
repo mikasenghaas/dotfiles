1c11bc175231b230fa819bec199fa93de69d37a4
mikasenghaas <accounts@jonassenghaas.de>
2023-03-24 17:02:36 +0100

    feat(nvim): enable copilot by default and alternative shorcut for accepting suggestion

--- .config/nvim/after/plugin/copilot.lua
+++ .config/nvim/after/plugin/copilot.lua
@@ -2,6 +2,20 @@
 --  configs for github copilot
 -- by: Mika Senghaas
 
--- disable by default and use <Tab> for completion
-vim.g.copilot_enabled = false
+-- use <Tab> for completion
 vim.g.copilot_assume_mapped = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+vim.g.copilot_filetypes = {
+  ["*"] = false,
+  ["markdown"] = true,
+  ["tex"] = true,
+  ["javascript"] = true,
+  ["typescript"] = true,
+  ["rust"] = true,
+  ["c"] = true,
+  ["c#"] = true,
+  ["c++"] = true,
+  ["go"] = true,
+  ["python"] = true,
+}
