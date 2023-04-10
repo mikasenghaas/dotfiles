dee4aac90601c9fc89b1a68e8924b63463556562
mikasenghaas <accounts@jonassenghaas.de>
2023-04-10 20:45:47 +0200

    feat(nvim): spell check and shortcut for quickly correcting last misspelled word in tex and md

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -1,4 +1,4 @@
--- settings if filetype tex
+-- settings if filetype .md
 vim.opt.textwidth = 80 -- 80 textwidth
 vim.opt.colorcolumn = "+1" -- colorcolumn one char after textwidth
 vim.opt.wrap = true  -- soft line wrapping
@@ -8,3 +8,4 @@ vim.opt.spelllang = 'en_gb' -- use british english
 -- correct previous spelling mistakes
 vim.api.nvim_set_keymap('i', '<C-l>', [[<Esc>[s1z=`]a]], {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-l>', [[[s1z=`]]], {noremap = true})
+
