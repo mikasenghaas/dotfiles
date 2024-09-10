b63a912a24d944c8c00bfe8a443cb4c46fe42c7d
mikasenghaas <mail@mikasenghaas.de>
2024-09-10 23:32:43 +0200

    Modified vim behaviour for Markdown and Latex

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,9 +2,21 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- .tex file settings
-vim.cmd([[
-  autocmd FileType tex setlocal textwidth=80
-  autocmd FileType tex setlocal colorcolumn=+1
-  autocmd FileType tex setlocal wrap
-]])
+-- LaTeX file settings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "tex" },
+  callback = function()
+    vim.opt_local.wrap = true -- Enable visual line wrapping
+    vim.opt_local.textwidth = 80 -- Set textwidth
+    vim.opt_local.colorcolumn = "80" -- Enable colorcolumn
+  end,
+})
+
+-- Markdown file settings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true -- Enable visual line wrapping
+    vim.opt_local.linebreak = true -- Wrap lines at word boundaries
+  end,
+})
